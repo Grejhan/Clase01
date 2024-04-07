08d9475a4fb86d72f018c47a353f5a64374ea65f
Victor Fernandez <manuel1508vc@gmail.com>
2024-04-06 23:02:03 -0500

    Agregar archivos de proyecto.

---
+++ SentenciasCondicionales/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// condicionales (if - switch )
+
+#region ejercicio01
+//string UsuarioAdmin, ClaveAdmin, usuarioInput, claveInput;
+
+
+//UsuarioAdmin = "Victor";
+//ClaveAdmin = "123456";
+
+//Console.WriteLine("Ingrese Usuario");
+//usuarioInput = Console.ReadLine();
+
+
+//Console.WriteLine("Ingrese Clave");
+//claveInput = Console.ReadLine();
+
+///**
+
+//        ==   --->  comparar valores si son iguales los valores
+//        =     ---> es para asignar, asignas el valor de la derecha al valor de la izquierda
+//        !=   ----> comapara si son diferentes
+//        < >= <= (numericos)
+//        ===   ---> comparar valores, compara el tipo de dato ( float(2.0) === decimal(2.0) )
+//        !()
+//        && || --->
+//*/
+
+
+//if (UsuarioAdmin == usuarioInput && ClaveAdmin == claveInput)
+//    Console.WriteLine("Bienvenido admin!");
+
+//else
+//    Console.WriteLine("Usuario y/o password incorecto!");
+
+
+#endregion
+
+Console.WriteLine("ingrese un numero");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero  == 0) {
+    Console.WriteLine("el numero ingresado  es igual a :" + numero );
+}
+else if (numero % 2 == 0)
+{
+    Console.WriteLine("El numero ingresado " + numero + " es par ");
+}
+else
+{
+    Console.WriteLine("En numero no es par");
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// comentarios de linea
+
+// variables numericas
+
+int numero ; // si quieres declarar variables iniciando con 0 se usa 0.0f , d , m
+float numero3 ; // cantidad de numero bajos 
+double numero2; // cantidad de numeros medio
+decimal numero4;  // acepta muchos mas numeros
+
+
+// resto de variables
+
+string nombreAlumno = "";
+string nombreAlumno2 = String.Empty;
+
+double promedio = 0;
+int nota1, nota2, nota3;
+
+nota1 = 20;
+nota2 = 15;
+nota3 = 14;
+
+promedio = (nota1 + nota2 + nota3) * 0.30;
+
+Console.WriteLine("El promedio de notas es: " + promedio);
+
+
+Console.WriteLine("Hello, World!");
